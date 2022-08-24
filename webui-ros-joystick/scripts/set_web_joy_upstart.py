7cd7410608ca4067d515c29871bbd0a613a6e285
pawelirh <pawel.irzyk@husarion.com>
2022-08-24 14:58:00 +0200

    Code formatted

--- webui-ros-joystick/scripts/set_web_joy_upstart.py
+++ webui-ros-joystick/scripts/set_web_joy_upstart.py
@@ -1,24 +1,25 @@
 #!/usr/bin/python3
 
-import sys
-import subprocess
 import os
+import subprocess
+import sys
 
 possible_arg_count = [2, 4]
-if (len(sys.argv) not in possible_arg_count) or len(sys.argv)==1:
-    sys.exit("""
+if (len(sys.argv) not in possible_arg_count) or len(sys.argv) == 1:
+    sys.exit(
+        """
     ###Mismatch argument count. Expected 3.###
 
-This script set up autostart of webui ros joystick 
-USAGE: 
+    Set up the autostart of webui ros joystick 
     sudo python3 set_web_joy_upstart.py <username> <local_ip_addr> <ros_master_ip/ros_master_hostname> 
     
-Example for default panther configuration: 
+    Example for default panther configuration: 
     sudo python3 set_web_joy_upstart.py husarion 10.15.20.2 10.15.20.3
-Uninstall: 
+    
+    Uninstall: 
     sudo python3 set_web_joy_upstart.py uninstall
-""")
-
+    """
+    )
 
 def prompt_sudo():
     ret = 0
@@ -27,28 +28,30 @@ def prompt_sudo():
         ret = subprocess.check_call("sudo -v -p '%s'" % msg, shell=True)
     return ret
 
-
 if prompt_sudo() != 0:
     sys.exit("The user wasn't authenticated as a sudoer, exiting")
 
 if str(sys.argv[1]) == "uninstall":
     subprocess.call("rm /usr/sbin/webui_ros_joy.sh", shell=True)
     subprocess.call("rm /etc/ros/env.sh", shell=True)
-    subprocess.call(
-        "rm /etc/systemd/system/webui_ros_joy.service", shell=True)
+    subprocess.call("rm /etc/systemd/system/webui_ros_joy.service", shell=True)
     subprocess.call("systemctl disable webui_ros_joy.service", shell=True)
     sys.exit("All removed")
-
 
 HOSTNAME = str(sys.argv[1])
 ROS_IP = str(sys.argv[2])
 ROS_MASTER_URI = str(sys.argv[3])
 
-
-print("Configuration ->", "Hostname:", HOSTNAME, "ROS_IP:", ROS_IP,
-      "ROS_MASTER_URI:", ROS_MASTER_URI)
+print(
+    "Configuration ->",
+    "Hostname:",
+    HOSTNAME,
+    "ROS_IP:",
+    ROS_IP,
+    "ROS_MASTER_URI:",
+    ROS_MASTER_URI,
+)
 subprocess.call("mkdir /etc/ros", shell=True)
-
 
 #
 # /etc/ros/env.sh
@@ -57,11 +60,11 @@ subprocess.call("mkdir /etc/ros", shell=True)
 env_msg = """#!/bin/sh
 export ROS_IP={rip} 
 export ROS_MASTER_URI=http://{rmu}:11311
-""".format(rip=ROS_IP, rmu=ROS_MASTER_URI)
+""".format(
+    rip=ROS_IP, rmu=ROS_MASTER_URI
+)
 
-subprocess.Popen(['echo "{}" > /etc/ros/env.sh'.format(env_msg)],  shell=True)
-
-
+subprocess.Popen(['echo "{}" > /etc/ros/env.sh'.format(env_msg)], shell=True)
 
 #
 # /usr/sbin/webui_ros_joy.sh
@@ -74,11 +77,13 @@ export ROS_HOME=$(echo ~{hn})/.ros
 roslaunch webui-ros-joystick webui.launch &
 PID=$!
 wait "$PID"
-""".format(hn=HOSTNAME)
+""".format(
+    hn=HOSTNAME
+)
 
 subprocess.Popen(
-    ['echo "{}" > /usr/sbin/webui_ros_joy.sh'.format(rap_script)],  shell=True)
-
+    ['echo "{}" > /usr/sbin/webui_ros_joy.sh'.format(rap_script)], shell=True
+)
 
 #
 # webui_ros_joy service
@@ -92,14 +97,16 @@ User={hn}
 ExecStart=/usr/sbin/webui_ros_joy.sh
 [Install]
 WantedBy=multi-user.target
-""".format(hn=HOSTNAME)
+""".format(
+    hn=HOSTNAME
+)
 
 subprocess.Popen(
-    ['echo "{}" > /etc/systemd/system/webui_ros_joy.service'.format(rap_service)],  shell=True)
-
+    ['echo "{}" > /etc/systemd/system/webui_ros_joy.service'.format(rap_service)],
+    shell=True,
+)
 
 subprocess.call("systemctl enable webui_ros_joy.service", shell=True)
 subprocess.call("chmod +x /usr/sbin/webui_ros_joy.sh", shell=True)
-
 
 print("Done!")
