f43d46843847a067cde94edae84e8759e5522590
Dawid <kmakd197@gmail.com>
2022-09-09 14:20:34 +0200

    quick fix

--- webui-ros-joystick/nodejs/public/js/update_view.js
+++ webui-ros-joystick/nodejs/public/js/update_view.js
@@ -2,87 +2,88 @@ let offset = 0;
 let create_e_stop = false;
 
 window.onload = function () {
-    console.log("onLoad triggered");
-    alert_container = document.getElementById("alerts");
-    socket = io();
-    socket.on("alert_states", function (alert_state) {
-        let alerts_section = new String();
-        alert_state.forEach((alert) => {
-            if (alert._state == 2) {
-                alerts_section += '<div class="alert alert-warning" role="alert">';
-                alerts_section += alert._message;
-                alerts_section += "</div>";
-            } else if (alert._state == 3) {
-                alerts_section += '<div class="alert alert-danger" role = "alert">';
-                alerts_section += alert._message;
-                alerts_section += "</div>";
-            }
-        });
-        alert_container.innerHTML = alerts_section;
+  console.log("onLoad triggered");
+  alert_container = document.getElementById("alerts");
+  socket = io();
+  socket.on("alert_states", function (alert_state) {
+    let alerts_section = new String();
+    alert_state.forEach((alert) => {
+      if (alert._state == 2) {
+        alerts_section += '<div class="alert alert-warning" role="alert">';
+        alerts_section += alert._message;
+        alerts_section += "</div>";
+      } else if (alert._state == 3) {
+        alerts_section += '<div class="alert alert-danger" role = "alert">';
+        alerts_section += alert._message;
+        alerts_section += "</div>";
+      }
     });
+    alert_container.innerHTML = alerts_section;
+  });
 
-    socket.on("create_e_stop", function (state) {
-        create_e_stop = state;
-        if (create_e_stop) {
-            setView();
-            socket.on("e_stop_state", function (state) {
-                let button = document.getElementById("eStopButton");
-                let buttonInput = document.getElementById("eStopInput");
-                let ledCircle = document.getElementById("ledCircle");
-                let joystick = document.getElementById("joystick");
+  socket.on("create_e_stop", function (state) {
+    create_e_stop = state;
+    let joystick = document.getElementById("joystick");
+    let ledCircle = document.getElementById("ledCircle");
+    if (create_e_stop) {
+      setView();
+      socket.on("e_stop_state", function (state) {
+        let button = document.getElementById("eStopButton");
+        let buttonInput = document.getElementById("eStopInput");
 
-                if (!state) {
-                    if (!button.disabled)
-                        setButton(button, buttonInput, "OFF");
-                        ledCircle.style.backgroundColor = LED_GREEN_COLOR;
-                        joystick.hidden = false;
-                }
-                else {
-                    if (!button.disabled)
-                        setButton(button, buttonInput, "ON");
-                        ledCircle.style.backgroundColor = LED_RED_COLOR;
-                        joystick.hidden = true;
-                }
-            });
-        } else {
-            offset = 0;
-            removeButton();
-            setView();
-            socket.off("e_stop_state");
+        if (!button.disabled) {
+          if (!state) {
+            setButton(button, buttonInput, "OFF");
+            ledCircle.style.backgroundColor = LED_GREEN_COLOR;
+            joystick.hidden = false;
+          } else {
+            setButton(button, buttonInput, "ON");
+            ledCircle.style.backgroundColor = LED_RED_COLOR;
+            joystick.hidden = true;
+          }
         }
-    })
+      });
+    } else {
+      offset = 0;
+      removeButton();
+      setView();
+      socket.off("e_stop_state");
+      joystick.hidden = false;
+      ledCircle.hidden = true;
+    }
+  })
 };
 
 $(window).resize(function () {
-    setView();
+  setView();
 });
 
 function setView() {
-    if (create_e_stop) {
-        let buttonSize = 100;
-        let toolBarHeight = buttonSize * 1.2;
-        offset = toolBarHeight / 2;
-        let toolBar = document.querySelector(".toolBar");
-        toolBar.style.height = toolBarHeight + "px";
+  if (create_e_stop) {
+    let buttonSize = 100;
+    let toolBarHeight = buttonSize * 1.2;
+    offset = toolBarHeight / 2;
+    let toolBar = document.querySelector(".toolBar");
+    toolBar.style.height = toolBarHeight + "px";
 
-        createButton(
-            $(window).width() - toolBarHeight / 1.5,
-            toolBarHeight / 2,
-            buttonSize
-        )
-    }
+    createButton(
+      $(window).width() - toolBarHeight / 1.5,
+      toolBarHeight / 2,
+      buttonSize
+    )
+  }
 
-    removeJoystick();
-    let joySize = 400;
-    if (joySize > $(window).height()) {
-        joySize = $(window).height();
-    }
-    if (joySize > $(window).width()) {
-        joySize = $(window).width();
-    }
-    createJoystick(
-        $(window).width() / 2,
-        $(window).height() / 2 + offset,
-        (joySize * 2) / 3
-    );
+  removeJoystick();
+  let joySize = 400;
+  if (joySize > $(window).height()) {
+    joySize = $(window).height();
+  }
+  if (joySize > $(window).width()) {
+    joySize = $(window).width();
+  }
+  createJoystick(
+    $(window).width() / 2,
+    $(window).height() / 2 + offset,
+    (joySize * 2) / 3
+  );
 }
